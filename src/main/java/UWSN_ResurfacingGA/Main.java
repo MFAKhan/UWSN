99b2c2c0fb2b4a9fc0be5ea9b2db9180718c1e81
saadkhan <skhan@mosaixsoft.com>
2016-03-23 22:44:37 -0700

    randomVariable change

--- src/main/java/UWSN_ResurfacingGA/Main.java
+++ src/main/java/UWSN_ResurfacingGA/Main.java
@@ -7,14 +7,14 @@ package UWSN_ResurfacingGA;
  *
  */
 public class Main {
-
-	public static void main(final String[] args) {
-		final GAforResurfacing resurface = new GAforResurfacing();
-		for (int i = 0; i < resurface.getSamplesPerTour(); i++) {
-			resurface.run();
-		}
-		resurface.analyzeExperiment();
-		resurface.printAverages();
-	}
+    int randomVariable = 0 ;
+    public static void main(final String[] args) {
+        final GAforResurfacing resurface = new GAforResurfacing();
+        for (int i = 0; i < GAforResurfacing.getSamplesPerTour(); i++) {
+            resurface.run();
+        }
+        resurface.analyzeExperiment();
+        resurface.printAverages();
+    }
 
 }
