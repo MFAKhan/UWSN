b0dd9201fdb45a80591f6809e38a93450effdbcb
FahadKhan <fahad.ahd@gmail.com>
2016-03-24 01:46:24 -0400

    Test Commit Hello

--- src/main/java/UWSN_ResurfacingGA/Main.java
+++ src/main/java/UWSN_ResurfacingGA/Main.java
@@ -2,19 +2,21 @@ package UWSN_ResurfacingGA;
 
 /**
  * This is the main file that is used by the GAResurface
- * 
+ *
  * @author Sehar Butt
  *
  */
 public class Main {
-    int randomVariable = 0 ;
-    public static void main(final String[] args) {
-        final GAforResurfacing resurface = new GAforResurfacing();
-        for (int i = 0; i < GAforResurfacing.getSamplesPerTour(); i++) {
-            resurface.run();
-        }
-        resurface.analyzeExperiment();
-        resurface.printAverages();
-    }
+	// hello
+	int randomVariable = 0;
+
+	public static void main(final String[] args) {
+		final GAforResurfacing resurface = new GAforResurfacing();
+		for (int i = 0; i < GAforResurfacing.getSamplesPerTour(); i++) {
+			resurface.run();
+		}
+		resurface.analyzeExperiment();
+		resurface.printAverages();
+	}
 
 }
