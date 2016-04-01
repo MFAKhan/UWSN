0805851fd3c7fc503bfbc65b3240d72536dbd3d2
saadkhan <saadkhan.ece@gmail.com>
2016-04-01 13:34:57 -0700

    Changes to put in the log4j appender

--- src/main/java/UWSN_ResurfacingGA/Main.java
+++ src/main/java/UWSN_ResurfacingGA/Main.java
@@ -1,8 +1,12 @@
 package UWSN_ResurfacingGA;
 
+import org.apache.log4j.BasicConfigurator;
+
 public class Main {
 
     public static void main(final String[] args) {
+        BasicConfigurator.configure();
+
         GAforResurfacing.getInstance().run();
     }
 
